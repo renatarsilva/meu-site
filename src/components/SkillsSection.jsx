e885dfac5b292ce1a5d871e78fb425642a65dc98
Renata <renata.rodriguesdasilva@outlook.com>
2025-06-29 01:11:15 -0300

    feat update skill level

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -33,7 +33,18 @@ export const SkillsSection = () => {
               key={key}
               className="bg-card p-6 rounded-lg shadow-xs card-hover"
             >
-              {" "}
+              <div className="text-left mb-4">
+                <h3 className="font-semibold">{skill.name}</h3>
+              </div>{" "}
+              <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
+                <div
+                  className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]"
+                  style={{ width: skill.level + "%" }}
+                />
+              </div>
+              <div>
+                <span>{skill.level}%</span>
+              </div>
             </div>
           ))}
         </div>
